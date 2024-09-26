aab7fd3f8d4a09279810e1e0cf1869cb4e8dc703
Jungle <zipoqiy@gmail.com>
2024-09-27 00:30:30 +0800

    perf: cool

--- nodejs/Index.js
+++ nodejs/Index.js
@@ -7,10 +7,10 @@ ipPool.acquireIP().then(res => {
 	console.log(res.data)
 	for (const d of res.data.data) {
 		ipPool.verifyIP(d.ip).then(res => {
-			console.log(res.data)
+			const netType = res.data.data.netWorkType
 			let visitWeb = new VisitWeb({
 				url: "https://4342473b18354702bda9aae9b0a2cd50.vip1.huaweicloudwaf.com/VZcQshay/14655.html",
-				ua: 'Mozilla/5.0 (Linux; Android 14; 2211133C Build/UKQ1.230804.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/126.0.6478.188 Mobile Safari/537.36 XWEB/1260117 MMWEBSDK/20240404 MMWEBID/5040 MicroMessenger/8.0.49.2600(0x28003133) WeChat/arm64 Weixin NetType/4G Language/zh_CN ABI/arm64'
+				ua: `Mozilla/5.0 (Linux; Android 14; 2211133C Build/UKQ1.230804.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/126.0.6478.188 Mobile Safari/537.36 XWEB/1260117 MMWEBSDK/20240404 MMWEBID/5040 MicroMessenger/8.0.49.2600(0x28003133) WeChat/arm64 Weixin NetType/${netType==='普通宽带'?'WIFI':'4G'} Language/zh_CN ABI/arm64`
 			})
 			const randomNumber = getRandomNumber(10000, 30000);
 			visitWeb.run({
